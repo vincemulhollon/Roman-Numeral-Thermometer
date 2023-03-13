00626ffb7d12b4d63d2d47aae392ac0c3bd95dd0
Vince Mulhollon <vince.mulhollon@springcitysolutions.com>
2023-03-13 17:31:46 -0500

    Async done, request complete.

--- Roman Numeral Thermometer/Program.cs
+++ Roman Numeral Thermometer/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Roman_Numeral_Thermometer;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        OutdoorTemperature temperature = new OutdoorTemperature();
+        Weather weather = new Weather();
+
+        await weather.Initialize;
+
+        if (weather.Initiated)
+        {
+            temperature.Celsius = weather.TemperatureCelsius();
+            Console.WriteLine("Note that -14C is about +7F aka VII in Roman Numerals");
+            Console.WriteLine(temperature.Roman());
+        }
+    }
+}
---
+++ Roman Numeral Thermometer/Weather.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace Roman_Numeral_Thermometer
+{
+    public class Weather
+    {
+        /// <summary>
+        /// Please await on Initialize and test Initiated before using object
+        /// </summary>
+        public bool Initiated { get; protected set; }
+        /// <summary>
+        /// Please await on Initialize and test Initiated before using object
+        /// </summary>
+        public Task Initialize { get; }
+
+        private double temperature = 0.0;
+
+        /// <summary>
+        /// Celsius temperature reported by National Weather Service API
+        /// </summary>
+        public double TemperatureCelsius()
+        {
+           return temperature;
+        }
+
+        public Weather()
+        {
+            // Class constructors can't return types,
+            // but async methods have to return a Task type.
+            // So, set Initialize using a method that returns a Task
+            // then over in main() you await off the Initialize
+            Initialize = CreateInstanceAsync();
+        }
+
+        private async Task CreateInstanceAsync()
+        {
+            using HttpClient client = new HttpClient();
+
+            // API described at:
+            // https://www.weather.gov/documentation/services-web-api#/default/station_observation_latest
+            // KUES is the airport by my house
+            client.BaseAddress =
+                new Uri("https://api.weather.gov/stations/kues/observations/latest?require_qc=false");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/geo+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "(springcitysolutions.com, vince.mulhollon@springcitysolutions.com)");
+
+            string responseBody = await client.GetStringAsync(client.BaseAddress);
+
+            // TODO: Parse the response
+            // Or, until then,
+            // console the response
+            // and change the temp to -14.0 C
+            Console.WriteLine(responseBody);
+            temperature = -14.0;
+
+            Initiated = true;
+        }
+
+    }
+}
